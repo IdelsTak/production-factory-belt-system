09400540a2e9f3547c6b50bd9d7f90c6faf8e8ae
Hiram K <hiram.kamau@outlook.com>
2023-05-16 08:33:21 +0300

    Update GameGrid.java

--- src/main/java/lms/grid/GameGrid.java
+++ src/main/java/lms/grid/GameGrid.java
@@ -4,10 +4,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * The GameGrid is responsible for managing the state and initialisation of the game's grid.
- * It provides the Map structure to hold the coordinates of each node in the grid. It also
- * maintains the size of the grid using a range variable. The range value donates how many
- * nodes each hexagonal grid node extends to.
+ * The GameGrid is responsible for managing the state and initialisation of the
+ * game's grid. It provides the Map structure to hold the coordinates of each
+ * node in the grid. It also maintains the size of the grid using a range
+ * variable. The range value donates how many nodes each hexagonal grid node
+ * extends to.
  *
  * @ass2
  * @version 1.1
@@ -17,11 +18,54 @@ import java.util.Map;
  */
 public class GameGrid {
 
+    private final int range;
+
     /**
-     * Helper method:
-     * Generates a grid with the given range, starting from the origin (the centre) and maintaining a
-     * balanced shape for the entire mapping structure.
-     * This has been provided to support you with the hexagonal coordinate logic.
+     * Create a new GameGrid with the given range, stored in a Map. A private
+     * helper method, generate() has been provided to assist you with the logic
+     * of the hexagonal coordinate system.
+     *
+     * @param range The range of the grid.
+     */
+    public GameGrid(int range) {
+        this.range = range;
+    }
+
+    /**
+     * Get a copy of the grid of the game.
+     *
+     * @return A copy of the grid of the game.
+     */
+    public Map<Coordinate, GridComponent> getGrid() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Get the range of the grid that was stored when the GameGrid instance was
+     * constructed.
+     *
+     * @return The range of the grid.
+     */
+    public int getRange() {
+        return range;
+    }
+
+    /**
+     * Set the GridComponent at the given coordinate.
+     *
+     * @param coordinate The coordinate of the GridComponent.
+     * @param component The GridComponent to be set.
+     */
+    public void setCoordinate(Coordinate coordinate, GridComponent component) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Helper method: Generates a grid with the given range, starting from the
+     * origin (the centre) and maintaining a balanced shape for the entire
+     * mapping structure. This has been provided to support you with the
+     * hexagonal coordinate logic.
+     *
      * @param range The range of the map.
      * @provided
      */
